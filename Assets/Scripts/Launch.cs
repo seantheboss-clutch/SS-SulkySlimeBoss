0deb051d72c8936ed2492da52f5c0114ac0b425a
CodeNinjasSJ <codeninja2198@outlook.com>
2024-01-17 16:33:53 -0800

    Line
    I need to edit the launch code to allow for horizontal and vertical rotations to be seen before launch, with the help of a cylinder guide.

--- Assets/Scripts/Launch.cs
+++ Assets/Scripts/Launch.cs
@@ -10,6 +10,7 @@ public class Launch : MonoBehaviour
     public Rigidbody playerRb;
     public Text hRotation;
     public Text vRotation;
+    public Text multT;
     public int speed;
     public int mult = 70;
     public Vector3 startPos;
@@ -39,85 +40,115 @@ public class Launch : MonoBehaviour
     {
         hRotation.text = yQuat.ToString();
         vRotation.text = xQuat.ToString();
-
+        multT.text = mult.ToString();
         if (launched == false)
         {
             if (Input.GetKey("left"))
             {
                 yQuat--;
-                print(yQuat);
+               /* print(yQuat);*/
             }
             if (Input.GetKey("right"))
             {
                 yQuat++;
-                print(yQuat);
-            }
+/*                print(yQuat);
+*/            }
         }
 
         if (Input.GetMouseButtonDown(1))
         {
             mult += 1;
             count += 1;
-            print(count);
-        }
+/*            print(count);
+*/        }
         if (Input.GetKey("up"))
         {
             xQuat++;
-            print(xQuat);
-        }
+/*            print(xQuat);
+*/        }
         if (Input.GetKey("down"))
         {
             xQuat--;
-            print(xQuat);
-        }
+/*            print(xQuat);
+*/        }
         //The player launches
         if (Input.GetMouseButtonDown(0) && count >= 1)
         {
-            print(count);
-            count = 0;
-            print(count);
-            rotation = Quaternion.Euler(-xQuat, yQuat, 0f);
+/*            print(count);
+*/            count = 0;
+/*            print(count);
+*/            rotation = Quaternion.Euler(-xQuat, yQuat, 0f);
             force = Vector3.forward;
             force = rotation * force;
             playerRb.velocity = transform.TransformDirection(force * speed * mult);
             playerRb.useGravity = true;
-            Invoke("reSpawn", 4f);
+            /*Invoke("reSpawn", 4f);*/
 
         }
 
     }
     private void OnCollisionEnter(Collision collision)
     {
-       if (collision.gameObject.tag == "obstacle")
-       {
-          mult = 0;
-          score += 100;
-          count = 0;
-          scoreText.text = score.ToString();
-          Destroy(collision.gameObject);
-          collision.gameObject.GetComponent<ObstacleBehaviors>().odeath = true;
-       }
-        score += 50;
-       if (collision.gameObject.tag == "collectable")
-       {
+        if (collision.gameObject.tag == "obstacle")
+        {
+            mult = 0;
+            score += 100;
+            count = 0;
+            scoreText.text = score.ToString();
+            if (score >= 600)
+            {
+                scoreText.color = Color.yellow;
+            } else if (score >= 400 && score <= 599)
+            {
+                scoreText.color = Color.grey;
+            } else if (score >= 200 && score <= 399)
+            {
+                scoreText.color = Color.red;
+            }
+            //player.GetComponent<ObstacleBehaviors>().odeath = true;
+            Destroy(collision.gameObject);
+            reSpawn(false);
+
+        }
+        else
+        {
+            reSpawn(true);
+        }
+        if(collision.gameObject.tag == "quad")
+        {
+            reSpawn(true);
+            score += 50;
+        }
+        if (collision.gameObject.tag == "collectable")
+        {
           score += 500;
 /*            SceneManager.LoadScene("Level2");
 */       }
        if(score >= 500)
-        {
-            SceneManager.LoadScene("Level2");
+       {
+            Scene sc = SceneManager.GetActiveScene();
+            if(sc.name == "Level1")
+            {
+                SceneManager.LoadScene("Level2");
+            } else
+            {
+                SceneManager.LoadScene("Menu");
+            }
+            
+            score = 0;
         }
-         
-       Invoke("reSpawn", 3f);
     }
  
-    void reSpawn()
+    void reSpawn(bool cond)
     {
         launched = false;
         playerRb.velocity = Vector3.zero;
         playerRb.useGravity = false;
         player.transform.position = startPos;
-        player.GetComponent<LivesManager>().death = true;
+        if(cond == true)
+        {
+            player.GetComponent<LivesManager>().death = true;
+        }
         count = 0;
     }
 
--- Assets/Scripts/SimpleT.cs
+++ Assets/Scripts/SimpleT.cs
@@ -7,23 +7,15 @@ using UnityEngine.SceneManagement;
 public class SimpleT : MonoBehaviour
 {
     public Text timer;
-    public int time = 6;
+    public float time = 6;
     // Start is called before the first frame update
     void Start()
     {
-        Timer();
-        Invoke("Stat", 6f);
-    }
+        Invoke("Stat",6f);
 
-    // Update is called once per frame
+    }
     void Stat()
     {
         SceneManager.LoadScene(2);
-    }
-    void Timer()
-    {
-       /* time -= Mathf.Round(Time.deltaTime);
-        timer.text = time.ToString();*/
-
     }
 }
